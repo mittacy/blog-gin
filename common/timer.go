64c230f40beb7d6627b10ac1958bc01c597cc0b6
MingTaoChen <mail@mittacy.com>
2020-06-14 11:37:09 +0800

    修复定时更新内存器缓存bug

--- common/timer.go
+++ common/timer.go
@@ -14,19 +14,17 @@ func StartTimer() {
 			// 1. 更新admin新增访问量到数据库
 			if err := cache.UpdateViewsToDatabase(); err != nil {
 				log.RecordErr(errors.New("更新博客新增访问量到数据库失败, err: " + err.Error()))
-			} else {
-				log.RecordErr(errors.New("更新博客新增访问量到数据库成功"))
 			}
 			// 2. 更新文章访问量到数据库
 			if err := cache.UpdateArticleViewsToDatabase(); err != nil {
 				log.RecordErr(errors.New("更新文章新增访问量到数据库失败, err: " + err.Error()))
 				log.RecordErr(err)
-			} else {
-				log.RecordErr(errors.New("更新文章新增访问量到数据库成功"))
 			}
-			// 3. 更新内存缓存
-
-			// 3. 计算下一次更新时间
+			// 3. 更新内存缓冲器缓存
+			if err := cache.InitCache(); err != nil {
+				log.ErrLogger.Fatalln(err)
+			}
+			// 4. 计算下一次更新时间
 			now := time.Now()
 			next := now.Add(time.Hour * 24)
 			next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
